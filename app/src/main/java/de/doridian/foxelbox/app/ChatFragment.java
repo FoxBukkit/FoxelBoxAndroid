47a7e525e59df5481f344b4a6daacd19806929ff
Doridian Draconia <mriq91@gmail.com>
2014-06-09 08:46:25 +0200

    Add message polling

--- app/src/main/java/de/doridian/foxelbox/app/ChatFragment.java
+++ app/src/main/java/de/doridian/foxelbox/app/ChatFragment.java
@@ -2,13 +2,11 @@ package de.doridian.foxelbox.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.*;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,7 +23,68 @@ public class ChatFragment extends Fragment {
             }
         });
 
+        ((ListView)fragmentView.findViewById(R.id.listChatMessages)).setAdapter(new ArrayAdapter<String>(fragmentView.getContext(), android.R.layout.simple_list_item_1));
+
+        chatReceiverQueue(fragmentView);
+
         return fragmentView;
+    }
+
+    private double lastTime = 0;
+
+    private class ChatPollWebUtility extends WebUtility {
+        private final View view;
+
+        private ChatPollWebUtility(View view) {
+            super(view.getContext());
+            this.view = view;
+        }
+
+        public void execute() {
+            if(LoginUtility.session_id == null) {
+                doRun();
+                return;
+            }
+
+            execute("message/poll", WebUtility.encodeData("since", "" + lastTime));
+        }
+
+        @Override
+        protected void onSuccess(JSONObject result) throws JSONException {
+            lastTime = result.getDouble("time");
+            final ListView chatMessageList = (ListView)view.findViewById(R.id.listChatMessages);
+            final ArrayAdapter<String> chatMessageListAdapter = (ArrayAdapter<String>)chatMessageList.getAdapter();
+            final JSONArray messages = result.getJSONArray("messages");
+            final int length = messages.length();
+            for(int i = 0; i < length; i++) {
+                JSONObject message = messages.getJSONObject(i);
+                String messagePlain = message.getJSONObject("contents").getString("plain");
+                chatMessageListAdapter.add(messagePlain);
+            }
+
+            doRun();
+        }
+
+        private void doRun() {
+            new Thread() {
+                public void run() {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) { }
+                    new ChatPollWebUtility(view).execute();
+                }
+            }.start();
+        }
+
+        @Override
+        protected void onError(String message) throws JSONException {
+            super.onError(message);
+            doRun();
+        }
+    }
+
+    public void chatReceiverQueue(View view) {
+        new ChatPollWebUtility(view).doRun();
     }
 
     public void sendChatMessage(View view) {
