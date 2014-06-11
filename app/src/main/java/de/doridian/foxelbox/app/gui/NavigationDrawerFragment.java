8a02576a9af3d6a9c439a9f8c10c43a0aa215f6d
Doridian Draconia <mriq91@gmail.com>
2014-06-11 10:54:02 +0200

    Don't update subtitle

--- app/src/main/java/de/doridian/foxelbox/app/gui/NavigationDrawerFragment.java
+++ app/src/main/java/de/doridian/foxelbox/app/gui/NavigationDrawerFragment.java
@@ -254,7 +254,7 @@ public class NavigationDrawerFragment extends Fragment {
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setSubtitle("");
+        //actionBar.setSubtitle("");
     }
 
     private ActionBar getActionBar() {
