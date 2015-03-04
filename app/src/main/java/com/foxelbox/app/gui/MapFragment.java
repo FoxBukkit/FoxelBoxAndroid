19f47085963c3d50d33b7172fa50a0912aa154a3
Mark Dietzer <mriq91@gmail.com>
2015-03-05 00:38:13 +0100

    Fix webview opening a new browser window

--- app/src/main/java/com/foxelbox/app/gui/MapFragment.java
+++ app/src/main/java/com/foxelbox/app/gui/MapFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.foxelbox.app.R;
 
 public class MapFragment extends MainActivity.PlaceholderFragment {
@@ -13,6 +15,8 @@ public class MapFragment extends MainActivity.PlaceholderFragment {
         final View fragmentView = inflater.inflate(R.layout.fragment_map, container, false);
 
         WebView webView = (WebView)fragmentView.findViewById(R.id.mapWebView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://api.foxelbox.com/map/main");
 
